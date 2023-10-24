0f2586b7696a373fe46939cd4053181cbbbfc0fe
stevie732 <144392044+stevie732@users.noreply.github.com>
2023-10-24 17:24:03 -0400

    "troubleunderstanding"

---
+++ Regex.py
@@ -0,0 +1,15 @@
+
+
+my_string = "Abraham Lincoln, Andrew P Garfield, Connor Milliken, Jordan Alexander Williams, Madonna, programming is cool, None, None"
+split_names = my_string.split(', ')
+print(split_names)
+
+
+
+
+
+
+
+
+
+
